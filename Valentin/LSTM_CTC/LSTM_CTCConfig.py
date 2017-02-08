44324b3946df696d190c34b6f217bb4fcd8ccc67
Zelgunn <valentin.durand.de.g@gmail.com>
2017-02-08 13:24:29 +0100

    Yolo

--- Valentin/LSTM_CTC/LSTM_CTCConfig.py
+++ Valentin/LSTM_CTC/LSTM_CTCConfig.py
@@ -1,6 +1,6 @@
 from configparser import SafeConfigParser
 
-class LSTM_CTCConfig(object):
+class LSTM_CTCModelConfig(object):
   def __init__(self, config_path = "model_config.ini"):
     config_parser = SafeConfigParser()
     config_parser.read(config_path)
@@ -23,3 +23,24 @@ class LSTM_CTCConfig(object):
 
     training_config = config_parser["Training"]
     self.learning_rate = float(training_config["learning_rate"])
+
+    saver_config = config_parser["Saver"]
+    self.checkpoints_path = saver_config["checkpoints_path"]
+
+class LSTM_CTCServerConfig(object):
+  def __init__(self, config_path = "server_config.ini"):
+    config_parser = SafeConfigParser()
+    config_parser.read(config_path)
+
+    listen_config = config_parser["Listen"]
+    self.listen_port = int(listen_config["listen_port"])
+    self.listen_addr = listen_config["listen_addr"]
+
+    send_config = config_parser["Send"]
+    self.send_port = int(send_config["send_port"])
+    self.send_addr = send_config["send_addr"]
+
+    sound_config = config_parser["Sound"]
+    self.sound_sample_rate = int(sound_config["sound_sample_rate"])
+
+
