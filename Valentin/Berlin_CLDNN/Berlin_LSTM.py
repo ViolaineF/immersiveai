74db5abc1f3339d93226ca60310476cebc111ba2
Zelgunn <valentin.durand.de.g@gmail.com>
2017-03-02 00:57:30 +0100

    Ajout padding2D des sequences

--- Valentin/Berlin_CLDNN/Berlin_LSTM.py
+++ Valentin/Berlin_CLDNN/Berlin_LSTM.py
@@ -1,21 +1,32 @@
 import numpy as np
 import tflearn
 
-from tflearn.data_utils import to_categorical
+from tflearn.data_utils import to_categorical, pad_sequences
 from Berlin_utils.BerlinDatabase import BerlinDatabase, build_timit_database
 
 MFCC_COUNT = 26
 PADDING = 200
 
+def pad_sequences_2d(sequences, maxlen, width, value):
+    tmp = []
+    for sequence in sequences:
+        tmp.append(np.reshape(sequence, [-1]))
+    tmp = pad_sequences(tmp, maxlen = maxlen * width, value = value)
+    tmp = np.reshape(tmp, [len(sequences), maxlen, width])
+    return tmp
+
 build_timit_database(r"E:\tmp\Berlin", MFCC_COUNT, 0.025, 0.025)
 database = BerlinDatabase(r"E:\tmp\Berlin", MFCC_COUNT)
 print("Loading database")
-database.load_batch(padding = PADDING)
+mfcc, mfcc_lengths, labels = database.load_batch()
 print("Database loaded")
 
-mfcc = database.mfcc_features
-labels = to_categorical(database.emotion_tokens, 5)
+print("Preprocessing : Padding MFCC")
+mfcc = pad_sequences_2d(mfcc, PADDING, MFCC_COUNT, 0)
+print("Preprocessing : Labels -> One hot")
+labels = to_categorical(labels, 5)
 
+print("Preprocessing : Shuffling")
 np.random.shuffle(mfcc)
 np.random.shuffle(labels)
 
@@ -25,11 +36,13 @@ train_labels = labels[:-100]
 test_mfcc = mfcc[-100:]
 test_labels = labels[-100:]
 
+print("Building network")
 net = tflearn.input_data([None, PADDING, MFCC_COUNT])
 net = tflearn.lstm(net, n_units = 128, dropout = 0.8, dynamic = True)
 net = tflearn.fully_connected(net, 5, activation = 'softmax')
 net = tflearn.regression(net, optimizer = "adam", learning_rate = 0.001, loss = "categorical_crossentropy")
 
+print("Building model")
 model = tflearn.DNN(net, tensorboard_verbose = 0)
+print("Training")
 model.fit(train_mfcc, train_labels, validation_set = (test_mfcc, test_labels), show_metric = True, batch_size = 8, n_epoch = 100)
-result = model.evaluate(test_mfcc, test_labels, batch_size = 100)
