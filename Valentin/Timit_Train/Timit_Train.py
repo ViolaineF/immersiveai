e3e212762949dcf8feb7aff0e63c9f9f6c75d3c8
Zelgunn <valentin.durand.de.g@gmail.com>
2017-03-02 17:32:49 +0100

    Update TimitTrain

--- Valentin/Timit_Train/Timit_Train.py
+++ Valentin/Timit_Train/Timit_Train.py
@@ -2,6 +2,8 @@ import numpy as np
 import tflearn
 import tensorflow as tf
 
+import math
+
 from tflearn.data_utils import to_categorical, pad_sequences
 from Timit_utils.TimitDatabase import TimitDatabase
 
@@ -27,12 +29,13 @@ def shuffle(inputs, labels):
     return inputs, labels
 
 database_path = r"C:\tmp\TIMIT"
+
 database = TimitDatabase(database_path)
 
 train_dataset = database.train_dataset
 test_dataset = database.test_dataset
 
-inputs_maxlen = 200
+inputs_maxlen = 150
 inputs_width = 40
 labels_maxlen = 75
 nb_classes = 62
@@ -46,21 +49,21 @@ train_mfcc = pad_sequences_2d(train_mfcc, inputs_maxlen, inputs_width, 0)
 test_mfcc = pad_sequences_2d(test_mfcc, inputs_maxlen, inputs_width, 0)
 
 print("Preprocessing : Padding outputs")
-train_labels = pad_sequences(train_labels, labels_maxlen)
-test_labels = pad_sequences(test_labels, labels_maxlen)
+train_labels = pad_sequences(train_labels, labels_maxlen, value = 61)
+test_labels = pad_sequences(test_labels, labels_maxlen, value = 61)
 
 print("Preprocessing : Labels -> One Hot")
 train_labels = to_categorical_2d(train_labels, nb_classes = nb_classes)
 test_labels = to_categorical_2d(test_labels, nb_classes = nb_classes)
 
 print("Preprocessing : Shuffling")
-shuffle(train_mfcc, train_labels)
-shuffle(test_mfcc, test_labels)
+#shuffle(train_mfcc, train_labels)
+#shuffle(test_mfcc, test_labels)
 
-conv_feat_count = 32
-max_pool_size = 3
-conv_result_size = conv_feat_count * int(inputs_width / max_pool_size)
-reduction_size = 128
+conv_feat_count = 16
+max_pool_size = 5
+conv_result_size = conv_feat_count * math.ceil(inputs_width / max_pool_size)
+reduction_size = 32
 
 print("Building network")
 input_data = tflearn.input_data([None, inputs_maxlen, inputs_width])
@@ -73,19 +76,27 @@ net = tflearn.reshape(net, [-1, inputs_maxlen * conv_result_size])
 net = tflearn.fully_connected(net, inputs_maxlen * reduction_size, activation = "relu")
 net = tflearn.reshape(net, [-1, inputs_maxlen, reduction_size])
 ### LSTM
-net = tflearn.merge([input_data, net], mode = "concat", axis = 2)
-net = tflearn.lstm(net, n_units = 256, dropout = 0.8, dynamic = False, return_seq = True)
-net = tf.transpose(tf.stack(net), [1,0,2])
-net = tflearn.lstm(net, n_units = 256, dropout = 0.8, dynamic = False, return_seq = False)
+#net = tflearn.merge([input_data, net], mode = "concat", axis = 2)
+#net = tflearn.lstm(net, n_units = 256, dropout = 0.8, dynamic = False, return_seq = True)
+#net = tf.transpose(tf.stack(net), [1,0,2])
+#net = tflearn.lstm(net, n_units = 256, dropout = 0.8, dynamic = False, return_seq = False)
 ### DNN
 net = tflearn.fully_connected(net, 512, activation = "relu")
+#net = tflearn.dropout(net, 0.8)
 net = tflearn.fully_connected(net, 512, activation = "relu")
-net = tflearn.fully_connected(net, labels_maxlen * nb_classes, activation = "softmax")
-net = tflearn.dropout(net, 0.5)
+net = tflearn.dropout(net, 0.8)
+net = tflearn.fully_connected(net, labels_maxlen * nb_classes, activation = "relu", bias = False)
 net = tflearn.reshape(net, [-1, labels_maxlen, nb_classes])
-net = tflearn.regression(net, optimizer = "adam", learning_rate = 0.001, loss = "categorical_crossentropy")
+    
+net = tflearn.regression(net, optimizer = "SGD", learning_rate = 0.1, loss = "softmax_categorical_crossentropy")
 
 print("Building model")
 model = tflearn.DNN(net, tensorboard_verbose = 0)
 print("Training")
-model.fit(train_mfcc, train_labels, validation_set = (test_mfcc, test_labels), show_metric = True, batch_size = 8, n_epoch = 100)
+model.fit(train_mfcc, train_labels, validation_set = (test_mfcc, test_labels), show_metric = True, batch_size = 64, n_epoch = 10000)
+x = model.predict(test_mfcc[50:60])
+print("########################")
+print(np.argmax(x, axis = 2))
+print("########################")
+print(np.argmax(test_labels[50:60], axis = 2))
+
